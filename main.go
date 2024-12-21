a7a975bb3752e8d91ab24769a209b21ce69360d1
Siam11651 <siam11651@outlook.com>
2024-12-21 11:50:36 +0600

    kaaj kori

--- main.go
+++ main.go
@@ -1,14 +1,55 @@
 package main
 
 import (
-	"findmydoc-backend/routes"
+	"database/sql"
+	"fmt"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
+
+	"findmydoc-backend/database"
+	"findmydoc-backend/routes"
 )
 
 func main() {
+	{
+		err := godotenv.Load()
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	r := gin.Default()
 
+	{
+		dbUser := os.Getenv("DB_USER")
+		dbPassword := os.Getenv("DB_PASSWORD")
+		dbName := os.Getenv("DB_NAME")
+		dbHost := os.Getenv("DB_HOST")
+		dbPort := os.Getenv("DB_PORT")
+		db, err := sql.Open(
+			"postgres",
+			fmt.Sprintf(
+				"user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+				dbUser,
+				dbPassword,
+				dbName,
+				dbHost,
+				dbPort,
+			),
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		database.Db = db
+	}
+
 	routes.RegisterRoutes(r)
-	r.Run(":8080")
+	r.Run(":3000")
 }
